94272fd1a19d7bb6c06dad2ccc994f3297cb084f
Michael McDuffee <mrmcduff@stripe.com>
2017-06-12 12:19:20 -0700

    breaking out the utility function changes needed for the Customer model

--- stripe/src/main/java/com/stripe/android/model/StripeJsonModel.java
+++ stripe/src/main/java/com/stripe/android/model/StripeJsonModel.java
@@ -4,9 +4,12 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.Size;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -26,14 +29,15 @@ public abstract class StripeJsonModel {
     }
 
     static void putStripeJsonModelMapIfNotNull(
-        @NonNull Map<String, Object> upperLevelMap,
-        @NonNull @Size(min = 1) String key,
-        @Nullable StripeJsonModel jsonModel){
+            @NonNull Map<String, Object> upperLevelMap,
+            @NonNull @Size(min = 1) String key,
+            @Nullable StripeJsonModel jsonModel){
         if (jsonModel == null) {
             return;
         }
         upperLevelMap.put(key, jsonModel.toMap());
     }
+
 
     static void putStripeJsonModelIfNotNull(
             @NonNull JSONObject jsonObject,
@@ -48,4 +52,36 @@ public abstract class StripeJsonModel {
         } catch (JSONException ignored) {}
     }
 
+    static void putStripeJsonModelListIfNotNull(
+            @NonNull Map<String, Object> upperLevelMap,
+            @NonNull @Size(min = 1) String key,
+            @Nullable List<? extends StripeJsonModel> jsonModelList) {
+        if (jsonModelList == null) {
+            return;
+        }
+
+        List<Map<String, Object>> mapList = new ArrayList<>();
+        for (int i = 0; i < jsonModelList.size(); i++) {
+            mapList.add(jsonModelList.get(i).toMap());
+        }
+        upperLevelMap.put(key, mapList);
+    }
+
+
+    static void putStripeJsonModelListIfNotNull(
+            @NonNull JSONObject jsonObject,
+            @NonNull @Size(min = 1) String key,
+            @Nullable List<? extends StripeJsonModel> jsonModelList) {
+        if (jsonModelList == null) {
+            return;
+        }
+
+        try {
+            JSONArray array = new JSONArray();
+            for (StripeJsonModel model : jsonModelList) {
+                array.put(model.toJson());
+            }
+            jsonObject.put(key, array);
+        } catch (JSONException ignored) {}
+    }
 }
